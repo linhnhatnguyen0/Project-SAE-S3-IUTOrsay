c2dc6c1bdfc0ad5e32f7914e2745e35f9f924886
linhnhatnguyen0 <51749625+linhnhatnguyen0@users.noreply.github.com>
2022-12-06 14:37:34 +0100

    14h37 06/12/2022

--- View/controlleur.php
+++ View/controlleur.php
@@ -3,6 +3,7 @@ include("./View/HeadAndBG.php");
 include("./View/Header/commonVisitorHeader.php");
 include("./View/search-tab.php");
 include("./View/search-result.php");
+include("./View/loan.php");
 include("./View/login.php");
 include("./View/footer.php");
 ?>
---
+++ View/loan-item.php
@@ -0,0 +1,36 @@
+<div class="loan-item">
+    <!-- <div class="check-box-item">
+        <label for="checkbox" class="checkbox-style">
+            <input type="checkbox" name="evondev" id="checkbox" class="checkbox-input" />
+            <div class="checkbox-box">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd" />
+                </svg>
+            </div>
+        </label>
+    </div> -->
+    <div class="loan-item-content">
+        <div class="loan-item-content-left">
+            <div class="loan-item-content-left-img">
+                <img src="./img/miserable.jpg" alt="">
+            </div>
+            <div class="loan-item-content-left-text">
+                <h1>Les Misérables</h1>
+                <p><b>Auteur: </b>Victor Hugo</p>
+                <p><b>Type: </b>Document</p>
+                <p><b>Exemplaire: </b>1A2B3C4B</p>
+            </div>
+        </div>
+        <div class="loan-item-content-right">
+            <span>3 jour retard</span>
+            <div class="expand">
+                <input type="checkbox" name="expand" id="expand">
+                <label for="expand">
+                    <ion-icon name="chevron-down-outline"></ion-icon>
+                </label>
+            </div>
+        </div>
+    </div>
+</div>
