265e3c2c5377a91cb2b9fb40bb4ec2c5d390e926
linhnhatnguyen0 <51749625+linhnhatnguyen0@users.noreply.github.com>
2022-12-30 12:11:45 +0100

    commit new

--- View/controlleur.php
+++ View/controlleur.php
@@ -1,6 +1,6 @@
 <?php
 include("./View/HeadAndBG.php");
-include("./View/Header/commonVisitorHeader.php");
+include("./View/Header/mainVisitorHeader.php");
 include("./View/search-tab.php");
 include("./View/search-result.php");
 include("./View/loan.php");
