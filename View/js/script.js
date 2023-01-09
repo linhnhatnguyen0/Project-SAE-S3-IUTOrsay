96a42aafad596eb745e1246f67a7bf79fe977b34
linhnhatnguyen0 <51749625+linhnhatnguyen0@users.noreply.github.com>
2023-01-09 17:36:05 +0100

    Update animation and scroll behav

--- View/js/script.js
+++ View/js/script.js
@@ -1,29 +1,26 @@
 let n = 0;
-let windowHeight = $(window).height();
+let windowHeight = $(window).height() + 20;
+let image1 = $(".img-1");
+let image2 = $(".img-2");
+let image3 = $(".img-3");
+let image4 = $(".img-4");
 $(document).scroll(function () {
-  var image1 = $(".img-1");
-  var image2 = $(".img-2");
-  var image3 = $(".img-3");
-  var image4 = $(".img-4");
   var imagePos = $(document).scrollTop();
-  //console.log(imagePos);
-  if (imagePos > 0) {
-    image1.animate({ left: -600 + "px" }, 100, "linear");
-    image2.animate({ right: -600 + "px" }, 100, "linear");
-    image4.animate({ right: -600 + "px" }, 100, "linear");
-    image3.animate({ bottom: 2000 + "px" }, 100, "linear");
-  }
-  if (imagePos == 0) {
-    image1.animate({ left: 0 + "px" }, 100, "linear");
-    image2.animate({ right: 0 + "px" }, 100, "linear");
-    image4.animate({ right: 100 + "px" }, 100, "linear");
-    image3.animate({ bottom: 0 + "px" }, 100, "linear");
-  }
+  console.log("imagePos=" + imagePos);
+  // if (imagePos > 0) {
+  //   image1.animate({ left: -600 + "px" }, 100, "linear");
+  //   image2.animate({ right: -600 + "px" }, 100, "linear");
+  //   image4.animate({ right: -600 + "px" }, 100, "linear");
+  //   image3.animate({ bottom: 2000 + "px" }, 100, "linear");
+  // }
+  // if (imagePos == 0) {
+  //   image1.animate({ left: 0 + "px" }, 100, "linear");
+  //   image2.animate({ right: 0 + "px" }, 100, "linear");
+  //   image4.animate({ right: 100 + "px" }, 100, "linear");
+  //   image3.animate({ bottom: 0 + "px" }, 100, "linear");
+  // }
 });
 $(document).bind("mousewheel DOMMouseScroll", function (event) {
-  if (n < 0) {
-    n = 0;
-  }
   if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
     $("header").animate({ top: 0 + "px" }, 100, "linear");
     $("html").animate({ scrollTop: n - windowHeight }, 100, "linear");
@@ -37,6 +34,23 @@ $(document).bind("mousewheel DOMMouseScroll", function (event) {
     if (n < 1500) {
       n += windowHeight;
     }
+  }
+  if (n < 0) {
+    n = 0;
+  }
+  if (n == 0) {
+    setTimeout(() => {
+      image1.animate({ left: 0 + "px" }, 100, "linear");
+      image2.animate({ right: 0 + "px" }, 100, "linear");
+      image4.animate({ right: 100 + "px" }, 100, "linear");
+      image3.animate({ bottom: 0 + "px" }, 100, "linear");
+    }, "100");
+  }
+  if (n != 0) {
+    image1.animate({ left: -600 + "px" }, 100, "linear");
+    image2.animate({ right: -600 + "px" }, 100, "linear");
+    image4.animate({ right: -600 + "px" }, 100, "linear");
+    image3.animate({ bottom: 2000 + "px" }, 100, "linear");
   }
 });
 $("#preferee-button-open").on("click", function () {
@@ -66,4 +80,5 @@ $("#connu-button-open").on("click", function () {
 $("#decouvrir-btn").on("click", function () {
   $("html").animate({ scrollTop: n + windowHeight }, 100, "linear");
   $("header").animate({ top: -100 + "px" }, 100, "linear");
+  n += windowHeight;
 });
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-require_once("./View/controlleur.php")
+require_once("./routeur.php")
     ?>
