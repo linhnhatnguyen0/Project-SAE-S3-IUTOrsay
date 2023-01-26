6e9929a6ca5665ebdfe1cf3c2b4eacab2d9c36ff
walidlec <wawa3ds@gmail.com>
2023-01-26 11:31:47 +0100

    getDocumentById

--- modele/modele.php
+++ modele/modele.php
@@ -6,5 +6,23 @@ class Modele
         return $this->$attribut;
     }
 
+    public function getDocumentById()
+    {
+        $requetePreparee = "SELECT * FROM Document WHERE NumDocument = :tag_NumDocument;";
+        $req_prep = Connexion::pdo()->prepare($requetePreparee);
+        $valeurs = array("tag_NumDocument" => $this->NumDocument);
+        try {
+          $req_prep->execute($valeurs);
+          $req_prep->setFetchMode(PDO::FETCH_CLASS, 'Document');
+          $v = $req_prep->fetch();
+          if (!$v)
+            return false;
+          return $v;
+        } catch (PDOException $e) {
+          echo "erreur : " . $e->getMessage() . "<br>";
+        }
+        return false;
+    }
+
 }
 ?>
