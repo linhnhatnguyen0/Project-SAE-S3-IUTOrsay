52d48810fc1407c4e93d04eceac42d71e213908b
DeiTy04 <104916298+DeiTy04@users.noreply.github.com>
2023-01-30 16:55:57 +0100

    Update server.php

--- View/server.php
+++ View/server.php
@@ -9,7 +9,7 @@
         <span class="v494_127">Vous offre les services</span>
         
         <div class="v494_126">
-            <div class="v483_125"><img src="../../img/livre-ouvert.png" alt="" class="v494_117"></div>
+            <div class="v483_125"><img src="./img/livre-ouvert.png" alt="" class="v494_117"></div>
             <a href="http://apple.com"  class="v483_262">
                 <span class="vbutton">Détail</span>
             </a>
@@ -17,21 +17,21 @@
             <span class="v494_103">Service d'emprunts</span>
         </div>
         <div class="v492_803">
-            <div class="v492_804"><img src="../../img/lost-items.png" alt="" class="v494_117"></div>
+            <div class="v492_804"><img src="./img/lost-items.png" alt="" class="v494_117"></div>
             <a href="http://apple.com"  class="v483_262">
                 <span class="vbutton">Détail</span>
             </a>
             <span class="v492_807">Services des livres perdus</span>
         </div>
         <div class="v492_797">
-            <div class="v492_798"> <img src="../../img/service-clients.png" alt="" class="v494_117"></div>
+            <div class="v492_798"> <img src="./img/service-clients.png" alt="" class="v494_117"></div>
             <a href="http://apple.com"  class="v483_262">
                 <span class="vbutton">Détail</span>
             </a>
             <span class="v492_801">Service d'handicape</span>
         </div>
         <div class="v492_791">
-            <div class="v492_792"> <img src="../../img/imprimante.png" alt="" class="v494_117"></div>
+            <div class="v492_792"> <img src="./img/imprimante.png" alt="" class="v494_117"></div>
             <a href="http://apple.com"  class="v483_262">
                 <span class="vbutton">Détail</span>
             </a>
