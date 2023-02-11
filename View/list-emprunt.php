488bc491aa3f99aba2d90b0bd528b7ed2c0b4e70
linhnhatnguyen0 <51749625+linhnhatnguyen0@users.noreply.github.com>
2023-02-11 14:35:44 +0100

    Create list-emprunt.php

---
+++ View/list-emprunt.php
@@ -0,0 +1,63 @@
+<section class="afficherEmprunt">
+    <h2>Emprunts</h2>
+    <div class="emprunts-rendu">
+        <div class="emprunt">
+            <div class="emprunt__titre">
+                <h3>Titre</h3>
+            </div>
+            <div class="emprunt__auteur">
+                <h3>Auteur</h3>
+            </div>
+            <div class="emprunt-id">
+                <h3>Id</h3>
+            </div>
+            <div class="emprunt__date">
+                <h3>Date</h3>
+            </div>
+            <div class="emprunt__retour">
+                <h3>Retour</h3>
+            </div>
+        </div>
+    </div>
+    <div class="emprunts-en-cours">
+        <div class="emprunt">
+            <div class="emprunt__titre">
+                <h3>Titre</h3>
+            </div>
+            <div class="emprunt__auteur">
+                <h3>Auteur</h3>
+            </div>
+            <div class="emprunt-id">
+                <h3>Id</h3>
+            </div>
+            <div class="emprunt__date">
+                <h3>Date</h3>
+            </div>
+            <div class="emprunt__retour">
+                <h3>Retour</h3>
+            </div>
+        </div>
+    </div>
+    <div class="emprunts-retard">
+        <div class="emprunt">
+            <div class="emprunt__titre">
+                <h3>Titre</h3>
+            </div>
+            <div class="emprunt__auteur">
+                <h3>Auteur</h3>
+            </div>
+            <div class="emprunt-id">
+                <h3>Id</h3>
+            </div>
+            <div class="emprunt__date">
+                <h3>Date</h3>
+            </div>
+            <div class="emprunt__retour">
+                <h3>Retour</h3>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+</section>
